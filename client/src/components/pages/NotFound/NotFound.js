e3b96934cb8d58da11675a270b481a8a018f4c1d
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-21 17:22:50 +0200

    Add 404 Page

--- client/src/components/pages/NotFound/NotFound.js
+++ client/src/components/pages/NotFound/NotFound.js
@@ -1,6 +1,19 @@
+import { Link } from 'react-router-dom';
+import notFoundImage from '../../../assets/svg/page-not-found.svg';
+import styles from './NotFound.module.css';
+
 function NotFound() {
     return (
-        <div>NotFound</div>
+        <section className='container'>
+            <div className={styles['not-found']}>
+                <div className={styles.image}>
+                    <img className='img' src={notFoundImage} alt="404" />
+                </div>
+                <h1>Ohh! Page Not Found</h1>
+                <p>We can't seem to find the page you're looking for</p>
+                <Link className='btn' to='/'>Back Home</Link>
+            </div>
+        </section>
     );
 }
 export default NotFound;
