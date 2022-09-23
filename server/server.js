9204c1d4b782104026c0fa72942b11cefcf0288f
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-23 10:39:26 +0200

    Add Auth route and controller

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 const PORT = process.env.PORT;
+import auth from './routes/auth.js';
 
 connectDB();
 const app = express();
@@ -10,5 +11,9 @@ const app = express();
 app.get('/', (req, res) => {
     res.json({ message: 'Jobster' });
 });
+
+// Routes
+app.use('/api/v1/auth', auth);
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
