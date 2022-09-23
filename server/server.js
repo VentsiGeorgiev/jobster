115ccb56652bef01fe5a896abfdf5d3a503e813e
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-23 10:17:49 +0200

    Connect to DB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 const PORT = process.env.PORT;
 
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
