eb27b0d5ea840e452c57c7d07927d1746443d9d0
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-23 12:09:38 +0200

    Error middleware and create user - server

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,41 @@
-const register = (req, res) => {
-    res.send('Register User');
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+
+const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            res.status(400);
+            throw new Error('All fields are required');
+        }
+
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            throw new Error('User Already exists');
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
+
 const login = (req, res) => {
     res.send('Login User');
 };
--- server/server.js
+++ server/server.js
@@ -4,9 +4,14 @@ dotenv.config();
 import connectDB from './config/db.js';
 const PORT = process.env.PORT;
 import auth from './routes/auth.js';
+import errorHandler from './middleware/error.js';
 
 connectDB();
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/', (req, res) => {
     res.json({ message: 'Jobster' });
@@ -15,5 +20,6 @@ app.get('/', (req, res) => {
 // Routes
 app.use('/api/v1/auth', auth);
 
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
