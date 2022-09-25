9d79dabca27faff3189c0bdc16784dc29e36104e
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-25 16:29:16 +0200

    Add Desktop sidebar links and styling

--- client/src/components/shared/LargeSidebar/LargeSidebar.js
+++ client/src/components/shared/LargeSidebar/LargeSidebar.js
@@ -1,4 +1,7 @@
+import { NavLink } from 'react-router-dom';
 import { useJobsContext } from '../../../context/jobsContext/jobsContext';
+import { AiOutlineFileSearch, AiOutlineFileAdd, AiOutlineUser } from 'react-icons/ai';
+import { ImStatsBars } from 'react-icons/im';
 import styles from './LargeSidebar.module.css';
 
 function LargeSidebar() {
@@ -9,8 +12,25 @@ function LargeSidebar() {
             {showSidebar &&
                 <aside className={showSidebar ? `${styles['sidebar-container']}` : styles.show}>
                     <div className={styles.content}>
-                        <h1>Sidebar</h1>
+                        <h2>Jobster</h2>
+                        <nav>
+                            <menu className={styles['nav-menu']}>
+                                <li>
+                                    <NavLink to='/'><ImStatsBars /> Stats</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='all-jobs'><AiOutlineFileSearch /> All Jobs</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='add-job'><AiOutlineFileAdd /> Add Job</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='profile'><AiOutlineUser /> Profile</NavLink>
+                                </li>
+                            </menu>
+                        </nav>
                     </div>
+
                 </aside>
             }
         </>
