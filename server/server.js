beba2d5c775b5a00e87d59738228198e41c1eeea
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-07 12:46:47 +0200

    Prepare for deploy;

--- server/server.js
+++ server/server.js
@@ -9,20 +9,35 @@ import errorHandler from './middleware/error.js';
 import notFoundMiddleware from './middleware/notFound.js';
 import cors from './middleware/cors.js';
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 connectDB();
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Jobster' });
-});
-
 // Routes
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/job', job);
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.json({ message: 'Jobster' });
+    });
+}
+
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandler);
